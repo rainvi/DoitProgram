d763bba9555b7f4607b10345d3fa1bb9fb1a3263
hongjw <hongjw030@naver.com>
2021-10-24 10:28:25 +0900

    for eng file

--- tech_scanning/filetransformer/EnNatural.py
+++ tech_scanning/filetransformer/EnNatural.py
@@ -1,17 +1,25 @@
 from TextExtract import *
 
+import os
+
 from nltk import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-import pandas as pd
-from bs4 import BeautifulSoup
 import re
 import nltk
-import matplotlib.pyplot as plt
-from matplotlib import rc
-import seaborn as sns
+
+
+#파일 확장자 확인하고 문자들 추출하기.
+text = TextExtract()
+file_path = "파일 경로 삽입."
+if os.path.splitext(file_path).equals(".pdf"):
+    extracted_text = text.get_pdf_text()
+elif os.path.splitext(file_path).equals(".hwp"):
+    extracted_text = text.get_hwp_text()
+elif os.path.splitext(file_path).equals(".docx"):
+    extracted_text = text.get_docx_text()
 
 #문장으로 잘라내기
-text = "TextExtract에서 받은 text."
+
 tokenized_sentences = sent_tokenize(text)
 tokenized_words = []
 for line in tokenized_sentences:
@@ -33,41 +41,3 @@ def text_cleaning(text):
     return ' '.join(stemmer_words)
 
 
-
-#######################################
-def show_tweet_word_count_stat(data):
-    num_word = []
-    num_unique_words = []
-    for item in data:
-        num_word.append(len(str(item).split()))
-        num_unique_words.append(len(set(str(item).split())))
-
-    # 일반
-    train['num_words'] = pd.Series(num_word)
-    # 중복 제거
-    train['num_unique_words'] = pd.Series(num_unique_words)
-
-    x = data[0]
-    x = str(x).split()
-    print(len(x))
-
-    rc('font', family='AppleGothic')
-
-    fig, axes = plt.subplots(ncols=2)
-    fig.set_size_inches(18, 6)
-    print('Tweet 단어 평균 값 : ', train['num_words'].mean())
-    print('Tweet 단어 중간 값', train['num_words'].median())
-    sns.distplot(train['num_words'], bins=100, ax=axes[0])
-    axes[0].axvline(train['num_words'].median(), linestyle='dashed')
-    axes[0].set_title('Tweet 단어 수 분포')
-
-    print('Tweet 고유 단어 평균 값 : ', train['num_unique_words'].mean())
-    print('Tweet 고유 단어 중간 값', train['num_unique_words'].median())
-    sns.distplot(train['num_unique_words'], bins=100, color='g', ax=axes[1])
-    axes[1].axvline(train['num_unique_words'].median(), linestyle='dashed')
-    axes[1].set_title('Tweet 고유한 단어 수 분포')
-
-    plt.show()
-
-
-show_tweet_word_count_stat(clean_processed_tweet)
--- tech_scanning/filetransformer/TextExtract.py
+++ tech_scanning/filetransformer/TextExtract.py
@@ -8,118 +8,110 @@ import olefile
 import zlib
 import struct
 
-try:
-    from xml.etree.cElementTree import XML
-except ImportError:
-    from xml.etree.ElementTree import XML
-import zipfile
+import win32com.client as win32
+
+class TextExtract:
+    ##pdf에서 글자 추출
+    def get_pdf_text(filename):
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = "utf-8"
+        laparams = LAParams()
+        device = TextConverter(
+            rsrcmgr,
+            retstr,
+            codec=codec,
+            laparams=laparams
+        )
+        fp = open(filename, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+
+        pages = PDFPage.get_pages(
+            fp,
+            pagenos,
+            maxpages=maxpages,
+            password=password,
+            caching=caching,
+            check_extractable=True
+        )
+        for page in pages:
+            interpreter.process_page(page)
+
+        text = retstr.getvalue()
+        fp.close()
+        device.close()
+        retstr.close()
+        return text
+
+    #hwp에서 text 얻기.
+    def get_hwp_text(filename):
+        f = olefile.OleFileIO(filename)
+        dirs = f.listdir()
+
+        # HWP 파일 검증
+        if ["FileHeader"] not in dirs or \
+                ["\x05HwpSummaryInformation"] not in dirs:
+            raise Exception("Not Valid HWP.")
+
+        # 문서 포맷 압축 여부 확인
+        header = f.openstream("FileHeader")
+        header_data = header.read()
+        is_compressed = (header_data[36] & 1) == 1
+
+        # Body Sections 불러오기
+        nums = []
+        for d in dirs:
+            if d[0] == "BodyText":
+                nums.append(int(d[1][len("Section"):]))
+        sections = ["BodyText/Section" + str(x) for x in sorted(nums)]
+
+        # 전체 text 추출
+        text = ""
+        for section in sections:
+            bodytext = f.openstream(section)
+            data = bodytext.read()
+            if is_compressed:
+                unpacked_data = zlib.decompress(data, -15)
+            else:
+                unpacked_data = data
+
+            # 각 Section 내 text 추출
+            section_text = ""
+            i = 0
+            size = len(unpacked_data)
+            while i < size:
+                header = struct.unpack_from("<I", unpacked_data, i)[0]
+                rec_type = header & 0x3ff
+                rec_len = (header >> 20) & 0xfff
+
+                if rec_type in [67]:
+                    rec_data = unpacked_data[i + 4:i + 4 + rec_len]
+                    section_text += rec_data.decode('utf-16')
+                    section_text += "\n"
+
+                i += 4 + rec_len
+
+            text += section_text
+            text += "\n"
+
+        return text
+
+    NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    PARA = NAMESPACE + 'p'
+    TEXT = NAMESPACE + 't'
 
 
+    def get_docx_text(filename):
+        word = win32.Dispatch("Word.Application")
+        word.Visible = 0
 
-#pdf에서 글자 추출
-def get_pdf_text(filename):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = "utf-8"
-    laparams = LAParams()
-    device = TextConverter(
-        rsrcmgr,
-        retstr,
-        codec=codec,
-        laparams=laparams
-    )
-    fp = open(filename, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
+        doc1 = word.Documents.Open(filename)
 
-    pages = PDFPage.get_pages(
-        fp,
-        pagenos,
-        maxpages=maxpages,
-        password=password,
-        caching=caching,
-        check_extractable=True
-    )
-    for page in pages:
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
-
-#hwp에서 text 얻기.
-def get_hwp_text(filename):
-    f = olefile.OleFileIO(filename)
-    dirs = f.listdir()
-
-    # HWP 파일 검증
-    if ["FileHeader"] not in dirs or \
-            ["\x05HwpSummaryInformation"] not in dirs:
-        raise Exception("Not Valid HWP.")
-
-    # 문서 포맷 압축 여부 확인
-    header = f.openstream("FileHeader")
-    header_data = header.read()
-    is_compressed = (header_data[36] & 1) == 1
-
-    # Body Sections 불러오기
-    nums = []
-    for d in dirs:
-        if d[0] == "BodyText":
-            nums.append(int(d[1][len("Section"):]))
-    sections = ["BodyText/Section" + str(x) for x in sorted(nums)]
-
-    # 전체 text 추출
-    text = ""
-    for section in sections:
-        bodytext = f.openstream(section)
-        data = bodytext.read()
-        if is_compressed:
-            unpacked_data = zlib.decompress(data, -15)
-        else:
-            unpacked_data = data
-
-        # 각 Section 내 text 추출
-        section_text = ""
-        i = 0
-        size = len(unpacked_data)
-        while i < size:
-            header = struct.unpack_from("<I", unpacked_data, i)[0]
-            rec_type = header & 0x3ff
-            rec_len = (header >> 20) & 0xfff
-
-            if rec_type in [67]:
-                rec_data = unpacked_data[i + 4:i + 4 + rec_len]
-                section_text += rec_data.decode('utf-16')
-                section_text += "\n"
-
-            i += 4 + rec_len
-
-        text += section_text
-        text += "\n"
-
-    return text
-
-NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
-PARA = NAMESPACE + 'p'
-TEXT = NAMESPACE + 't'
-
-
-def get_docx_text(filename):
-    document = zipfile.ZipFile(filename)
-    xml_content = document.read('word/document.xml')
-    document.close()
-    tree = XML(xml_content)
-    paragraphs = []
-    for paragraph in tree.getiterator(PARA):
-        texts = [node.text
-                for node in paragraph.getiterator(TEXT)
-                if node.text]
-        if texts:
-            paragraphs.append(''.join(texts))
-    return '\n\n'.join(paragraphs)
+        test = str(doc1.Content)
+        test = test.replace('\r', '\n')
+        word.Quit()
+        return test
