9fcea3ed707a54fe0f0d6bbbd09929c48cca3b7e
hongjw <hongjw030@naver.com>
2021-11-15 01:20:28 +0900

    data cleaner 생성, hwp -> pdf 함수 미완

--- tech_scanning/filetransformer/EngEmbedding.py
+++
@@ -1,7 +0,0 @@
-##언어모델:
-
-from EngPreprocess import *
-
-
-#정제된 text 배열 가져오기.
-preprocessed_text = EngPreprocess().propressing()
--- tech_scanning/filetransformer/Main.py
+++
@@ -1,8 +0,0 @@
-from TextExtract import *
-from EngPreprocess import *
-
-my_text_path = "C:/Users/wonai/mystatus/Doit_program/tech_scanning/filetransformer/practice.hwp"
-
-##te = TextExtract(my_text_path)
-epp = EngPreprocess(my_text_path)
-print(epp.propressing())
