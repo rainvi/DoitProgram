f3b5e2f5e0e01987ef44fbc223b4a12b0c2393e7
rainvi <89857287+rainvi@users.noreply.github.com>
2021-12-02 22:46:37 +0900

    Update webCrawling.py

--- webCrawling/webCrawling.py
+++ webCrawling/webCrawling.py
@@ -2,15 +2,15 @@
 # 크롤링팀원 문정빈입니다!
 # 일괄 파일 다운 구현했습니다. 파일이 업로드되지 않은 칸은 건너뛸 수 있도록 했어요.
 # 강의 수가 많은 학기는 부가조건이 필수라, 여러가지를 따져본 결과 교과목 구분을 기준으로 조건을 나누기로 했어요. (다른 조건은 강의가 중복이 많거나 누락되는 강의가 발생했습니다!)
-# 다운받을 때 파일명을 변경해서 저장하도록 했어요. 교수님들께서 파일명 정말 다양하게 올려주셔서 일괄 형식으로 저장하면 편할 것 같아서요!
-# 미구현 및 오류 : 돋보기 아이콘으로 된 파일은 아직 저장하지 못해요. -> 이것때문에 파일명이 꼬이는 경우가 있는데 조만간 수정할 예정입니다!
-# count가 1인 파일만 저장을 안하는데, 뭐가 문제인지 살펴볼 예정이에요.
-# 한 페이지를 다 저장하고 다음 조건(교과목구분)으로 넘어갈 때, 강의 수 차이로 인해 스크롤이 사라지는 것 때문에 오류가 발생하고 있어요. 조만간 해결할 예정이에요.
+# 다운받을 때 파일명을 변경해서 저장하도록 했어요. 교수님들께서 파일명 정말 다양하게 올려주셔서 일괄 형식으로 저장하면 편할 것 같아서요! (확인된 형식 : pdf, hwp, docx, doc, ppt, png)
 
 from bs4 import BeautifulSoup  # cmd의 pip명령을 통해 따로 깔아주어야 해요
 from selenium import webdriver  # 이것두! webdriver은 따로 설치해야 합니다
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains       #새탭클릭
 from selenium.webdriver.common.keys import Keys
+from PIL import Image #pip install pillow
+from Screenshot import Screenshot_Clipping #pip install selenium_screenshot
 import time
 import os
 import shutil
@@ -21,7 +21,6 @@ url2 = "http://localhost:8000/"
 
 print("2021 - 겨울계절1차 : 1, 2021 - 2학기 : 2, ... , 2015 - 1학기 : 28 \n 반드시 숫자로만 입력해야 합니다! \n")
 semesterNum = input("학기를 설정해주세요. (1~28) : ")
-#시간상 테스트는 1만 돌렸습니다
 
 # 크롬 브라우저로 웹 실행
 path = "D:\python\chromedriver_win32 (1)\chromedriver.exe"
@@ -61,10 +60,11 @@ element 찾는 법은 ctrl+F로 검색하면 빠릅니다. ---> 스크롤이 생
 '''
 #교과목 구분 설정하기
 # 귀찮게 설정하는 이유 : 교과목이 너무 많아서 사이트 자체에서 조건을 하나 이상 설정하게끔 막아놨습니다. 스크롤이 터지는 걸 방지하기 위한 것 같아요
-# 교과목 구분에 따라 교양/비사대교직/전공선택(교직)/대학기초/전공기초/전공/학점교환 검색 후 합치기
-# 계절학기는 그냥 조회해도 보이지만 구별하는 게 더 귀찮다
+# 교과목 구분에 따라 교양/비사대교직/전공선택(교직)/대학기초/전공기초/전공 검색 후 합치기
+# 학점교환의 경우 폼이 달라서 나중에 추가하기로 했어요! 애초에 강계가 없어서 db만 있으면 될 것 같은
 
-for i in range(1, 9):
+# 교과목 구분 combobox 클릭하기
+for i in range(1, 7):
     # 교과목 구분 combobox 클릭하기
     driver.find_element_by_xpath(
         '//*[@id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_cbbViewKindCd_comboedit_input"]'
@@ -119,10 +119,13 @@ for i in range(1, 9):
          while(Temp == 1):
              Total +=1
              if (Total > tempText):
+                 print("다 돌았다\n")
                  break
              if soup.find("div",id ='mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_'+str(count)+'_cell_'+str(count)+'_0') == None:
+                 print("이 행이 없는 것 같다\n")
                  break
              else:
+                 #어떻게 이름을 저장할지 미리 정해둠
                  className = driver.find_element_by_id(
                      'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
                          count) + '_cell_' + str(count) + '_3').text
@@ -133,27 +136,70 @@ for i in range(1, 9):
                      'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
                          count) + '_cell_' + str(count) + '_8').text
                  #가끔 교수명에 \n가 들어가는 경우가 있어서... 치환
+                 #예외 > \, /, :, *, <, >, | 강의명에 포함되면 안됨ㅠ
                  className = className.replace("/","+")
+                 className = className.replace("\\", "+")
+                 className = className.replace(":", "-")
+                 className = className.replace("*", "+")
+                 className = className.replace("<", "[")
+                 className = className.replace(">", "]")
+                 className = className.replace("|", "+")
+                 className = className.replace("\n", "")
                  professor = professor.replace("/","+")
+                 professor = professor.replace("\\", "+")
+                 professor = professor.replace(":", "-")
+                 professor = professor.replace("*", "+")
+                 professor = professor.replace("<", "[")
+                 professor = professor.replace(">", "]")
+                 professor = professor.replace("|", "+")
                  professor = professor.replace("\n","")
                  new_filename = className + "_" + classNum + "_" + professor + "_"
 
                  print("count: "+str(count))
 
-                 # 하나의 강의에 모든 파일 받기?
-                 # 오류 : 어째선지 count가 1일 때의 행은 다운이 안되어서... 이유를 찾고 있습니다!
+                 # 하나의 강의에 들은 모든 파일 받기
+
                  for number in range(14, 18):
 
-                     if soup.find('div', id = 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_'+str(count)+'_cell_'+str(count)+'_'+str(number)+'_controlexpandImageElement') == None:
+                     # 현재 html 가져오기 이걸 넣어야 find가 제대로 작동함
+                     soup = BeautifulSoup(driver.page_source, "html.parser")
+
+                     if soup.find('div', id = 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_'
+                                              +str(count)+'_cell_'+str(count)+'_'+str(number)+'_controlexpand') == None:
+                         print(str(number)+" 칸이 비었다 \n")
                          pass
 
                      else:
+
                          # 파일 다운 버튼을 클릭
+                         # 14 15일 경우 돋보기 16일 경우 파일 17일 경우 모르겠다... 올린 분이 안계시는 것 같애요
                          number = str(number)
 
-                         driver.find_element_by_id(
-                             'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
-                                 count) + '_cell_' + str(count) + '_' + number).click()
+                         if number == "14" or number =="15":
+                             print("돋보기 칸 찾았다\n")
+                             target = driver.find_element_by_id(
+                                'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
+                                    count) + '_cell_' + str(count) + '_' + number)
+                             ActionChains(driver).key_down(Keys.CONTROL).click(target).key_up(Keys.CONTROL).perform()
+                             time.sleep(2)
+                             driver.switch_to.window(driver.window_handles[1])
+
+                             ss = Screenshot_Clipping.Screenshot()
+                             ss.full_Screenshot(driver, save_path= downloadPath, image_name= r"\temp.png")
+                             # 하단이 살짝 잘려서 저장되는데 이유를 모르겠네요ㅠㅠ
+
+                             driver.close()
+                             driver.switch_to.window(driver.window_handles[0])
+
+                         if number == "16":
+                             print("파일 칸 찾았다\n")
+                             driver.find_element_by_id(
+                                'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
+                                    count) + '_cell_' + str(count) + '_' + number).click()
+
+                         if number == "17" :
+                            print("URL 찾았다~ \n")
+
 
                          time.sleep(2)  # 없으면 오류남 (파일이 제대로 다운이 안되거나...)
 
@@ -171,21 +217,27 @@ for i in range(1, 9):
 
                          print(filename)
                          list_filename = filename.split(".")
-                         fileEx = list_filename[1]
+                         lenmax = len(list_filename)
+                         fileEx = list_filename[lenmax-1]
                          print(new_filename + number + "." + fileEx)
                          shutil.move(os.path.join(downloadPath, filename),
                                      os.path.join(downloadPath, new_filename + number + "." + fileEx))
                          time.sleep(1)
 
                 # 브라우저 스크롤 가장 밑으로
-                 for i in range(0, 4):
-                    driver.find_element_by_id('mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_incbuttonAlignImageElement').click()
+
+                 attr = {'id': 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_incbuttonAlignImageElement',
+                         'style': 'user-select: none; position: absolute; left: 0px; top: 0px; width: 15px; height: 15px; background-repeat: no-repeat; background-position: center center; background-image: url("/eureka/my/nxc/_theme_/theme/base/scr_WF_VerincN.png");'}
+                 if soup.find('div', attrs=attr) != None:
+                    for i in range(0, 4):
+                        driver.find_element_by_id('mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_incbuttonAlignImageElement').click()
 
 
                  # 스크롤 1칸 내리기
-                 # 스크롤이 없을 수도 있어서 if문 넣었는데... 작동을 안해서 이 부분 좀 더 다른 방법을 고민해보겠어요
 
-                 if soup.find('div', id = 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_vscrollbar_incbuttonAlignImageElement') != None:
+                 attr = {'id':'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_vscrollbar_incbutton', 'style' : 'user-select: none; position: absolute; overflow: hidden; left: 0px; top: 478px; width: 15px; height: 15px; border-radius: 0px; background-color: transparent; cursor: pointer;'}
+
+                 if soup.find('div', attrs = attr) != None:
                      driver.find_element_by_id(
                          'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_vscrollbar_incbuttonAlignImageElement').click()
 
@@ -224,12 +276,11 @@ django 서버에 업로드할 수 있어야함.
 2) download link를 얻는 방법을 알아내기
 => 1)의 경우 mysql에 저장하는 내용은 가상환경의 파일경로가 될 것이고, 2)의 경우 mysql에 저장하는 내용은 download link가 될 것입니다.
 
-2. Chrome 창으로 뜨는 파일(돋보기) 저장 구현하기 => 출력 눌러서 pdf 저장하는 기능 이용하면 될 것 같다.
-3. 중복처리... 파일명 아니까 쉬울듯 pass로 처리하거나
+2. 중복처리 -> 하지 않기로 함. 무조건 다 갱신하는 것이 더 효율이 좋을 것 갱신도 해야하구...
 
 * 구동시간에 대하여 *
 이 코드는 오류를 방지하기 위해 sleep을 많이 사용하고 있기도 하지만,
-사람이 클릭하듯이 돌아가기 때문에 구동 시간이 깁니다.
-하지만 저희가 만드는 어플은 zoom 회의와 같이 실시간과 속도가 매우 중요한 서비스가 아니므로,
-지금의 속도로도 충분할 것이라고 생각됩니다.
+사람이 클릭하듯이 돌아가기 때문에 구동 시간이 깁니다. (한 과목 저장에 대략 4초정도? 걸리는 것 같아요...)
+한 번 한 학기를 갱신하는 데 2~3시간 쯤 걸릴까요... 강의 수가 2000개라고 가정하면
+줄일 수 있다면 좋겠지만 속도가 중요한 시스템이 아니니 괜찮을 것 같기도 합니다.
 """
